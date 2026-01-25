10f29d6b794054a90966b8a195e2122c56bc8005
Sachin <sacihnminde9359@gmail.com>
2026-01-26 00:35:18 +0530

    completed upto login into file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,16 +9,4 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-//    @Bean
-//    public RouterFunction<ServerResponse> customRoutes() {
-//        // @formatter:off
-//        return route("path_route")
-//                .GET("/get", http())
-//                .before(uri("https://httpbin.org"))
-//                .build().and(route("host_route")
-//                        .route(host("*.myhost.org"), http())
-//                        .before(uri("https://httpbin.org"))
-//                        .build());
-//    }
 }
